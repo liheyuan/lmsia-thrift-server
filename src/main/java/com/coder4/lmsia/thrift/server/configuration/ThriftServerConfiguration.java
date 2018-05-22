fb06d2ab3abdde00af469efc2c0535f93c9a405c
coder4 <lihy@fenbi.com>
2018-05-22 11:23:20 +0800

    MOD: 0.0.2

--- src/main/java/com/coder4/lmsia/thrift/server/configuration/ThriftServerConfiguration.java
+++ src/main/java/com/coder4/lmsia/thrift/server/configuration/ThriftServerConfiguration.java
@@ -4,24 +4,20 @@ import com.coder4.lmsia.thrift.server.ThriftServerRunnable;
 import org.apache.thrift.TProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author coder4
  */
 @Configuration
 @ConditionalOnBean(value = {TProcessor.class})
-public class ThriftServerConfiguration implements InitializingBean, DisposableBean {
+public class ThriftServerConfiguration implements InitializingBean {
 
     private Logger LOG = LoggerFactory.getLogger(ThriftServerConfiguration.class);
-
-    private static final int GRACEFUL_SHOWDOWN_SEC = 3;
 
     @Autowired
     private TProcessor processor;
@@ -30,20 +26,21 @@ public class ThriftServerConfiguration implements InitializingBean, DisposableBe
 
     private Thread thread;
 
-    @Override
-    public void destroy() throws Exception {
-        // TODO graceful shutdown
-
-        LOG.info("Wait for graceful shutdown on destroy(), {} seconds", GRACEFUL_SHOWDOWN_SEC);
-        Thread.sleep(TimeUnit.SECONDS.toMillis(GRACEFUL_SHOWDOWN_SEC));
-        LOG.info("Shutdown rpc server.");
-        thriftServer.stop();
-        thread.join();
+    @Bean(name = "shutdownThriftServerRunnable")
+    public Runnable shutdownThriftServerRunnable() {
+        return () -> {
+            LOG.info("Shutdown thrift server.");
+            try {
+                thriftServer.stop();
+                thread.join();
+            } catch (Exception e) {
+                LOG.error("Shutdown thrift server error", e);
+            }
+        };
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-
         thriftServer = new ThriftServerRunnable(processor);
         thread = new Thread(thriftServer);
         thread.start();
